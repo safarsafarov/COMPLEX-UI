071f38d17c8c626edd1d22dbcac1c4a852ecef29
safarsafarov <safarov.safar@hotmail.com>
2021-05-23 01:22:32 +0800

    commit

---
+++ complex-UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  complex-UI
+//
+//  Created by codestorm on 2021/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ complex-UI/complex_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  complex_UIApp.swift
+//  complex-UI
+//
+//  Created by codestorm on 2021/5/25.
+//
+
+import SwiftUI
+
+@main
+struct complex_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
